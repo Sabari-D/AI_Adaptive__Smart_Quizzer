09b169e5028068e5008cfcc408d3a70eef31393a
Sabari-D <sabaridhandapani69@gmail.com>
2025-11-24 06:43:33 +0530

    Refactor main.ts to improve code formatting

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
-// D:\SmartQuizzer\frontend\vue-project\src\main.ts
+
 
 import './assets/main.css'
 
 import { createApp } from 'vue'
-// Ensure Pinia is imported from the pinia package
+
 import { createPinia } from 'pinia' 
 import App from './App.vue'
-// Ensure router is imported from the local router file
+
 import router from './router' 
 
 const app = createApp(App)
@@ -14,5 +14,6 @@ const pinia = createPinia()
 
 app.use(pinia) 
 app.use(router)
+
 
 app.mount('#app')
